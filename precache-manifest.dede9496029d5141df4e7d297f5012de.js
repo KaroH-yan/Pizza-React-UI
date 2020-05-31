317cc78aeb4c672e837489525e210fb79abf8487
Karo <karo.hambardzumyan@cherryc.ai>
2020-05-31 03:38:10 -0700

    Updates

---
+++ precache-manifest.dede9496029d5141df4e7d297f5012de.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8c7fb5fe5a38c4d75f9e4db4b3599b",
+    "url": "/Pizza-React-UI/index.html"
+  },
+  {
+    "revision": "80ba432c40b775f399e2",
+    "url": "/Pizza-React-UI/static/css/2.f0db2a3b.chunk.css"
+  },
+  {
+    "revision": "ddd60f7e174cf7364d41",
+    "url": "/Pizza-React-UI/static/css/main.2453da4f.chunk.css"
+  },
+  {
+    "revision": "80ba432c40b775f399e2",
+    "url": "/Pizza-React-UI/static/js/2.f5410ab2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Pizza-React-UI/static/js/2.f5410ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd60f7e174cf7364d41",
+    "url": "/Pizza-React-UI/static/js/main.21ec064c.chunk.js"
+  },
+  {
+    "revision": "053bcbac355c2f8d8600",
+    "url": "/Pizza-React-UI/static/js/runtime-main.f3c03765.js"
+  },
+  {
+    "revision": "c25e368178e628d5d5a8ce946694e672",
+    "url": "/Pizza-React-UI/static/media/background-pizza.c25e3681.png"
+  },
+  {
+    "revision": "f17f6cd0bf1a1e57db3b0b019c8056bb",
+    "url": "/Pizza-React-UI/static/media/bannerPizza.f17f6cd0.jpg"
+  },
+  {
+    "revision": "f6c2b790cedaa4c4f8ed3084eecf2274",
+    "url": "/Pizza-React-UI/static/media/bannerPizza2.f6c2b790.jpg"
+  },
+  {
+    "revision": "d1c0061a1413dd72e84c2b104194fe72",
+    "url": "/Pizza-React-UI/static/media/coca-cola-zero.d1c0061a.png"
+  },
+  {
+    "revision": "8c8548da797960f939d9510731ae4303",
+    "url": "/Pizza-React-UI/static/media/coca-cola.8c8548da.png"
+  },
+  {
+    "revision": "135e70f50b5f5dc44064e931134dae08",
+    "url": "/Pizza-React-UI/static/media/fanta.135e70f5.png"
+  },
+  {
+    "revision": "b294adac3f6f61ad400f3ac0cd5acda3",
+    "url": "/Pizza-React-UI/static/media/pizza-1.b294adac.png"
+  },
+  {
+    "revision": "3440821fb7fb837e98d392a626c6a3e6",
+    "url": "/Pizza-React-UI/static/media/pizza-2.3440821f.png"
+  },
+  {
+    "revision": "8d0ff1321945245a02ccd6cc7fb85ae1",
+    "url": "/Pizza-React-UI/static/media/pizza-3.8d0ff132.png"
+  },
+  {
+    "revision": "2763afce25d9305673424b3b25595763",
+    "url": "/Pizza-React-UI/static/media/pizza-4.2763afce.png"
+  },
+  {
+    "revision": "91a862ebd8b7bba5787940a94744db0c",
+    "url": "/Pizza-React-UI/static/media/pizzabanner3.91a862eb.jpg"
+  },
+  {
+    "revision": "21fc4e31c271835d9065ff669fb61636",
+    "url": "/Pizza-React-UI/static/media/pizzabanner5.21fc4e31.jpg"
+  },
+  {
+    "revision": "18296005927e6e22eef8d4dfbb85487a",
+    "url": "/Pizza-React-UI/static/media/sprite.18296005.png"
+  }
+]);
